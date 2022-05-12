8e39c90e4d76c6a8c19b66917db3967109c70978
CGAnVil <hoangminh.tlu@gmail.com>
2022-05-12 14:29:35 +0700

    first upload

---
+++ src/main/java/com/codegym/BlogSickteamM6Application.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSickteamM6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSickteamM6Application.class, args);
+    }
+
+}
