49d606ff8aab4b5c1d45ad83091d0e347e3ca86f
CGAnVil <hoangminh.tlu@gmail.com>
2022-05-12 14:53:13 +0700

    user+role model

--- src/main/java/com/codegym/model/User.java
+++ src/main/java/com/codegym/model/User.java
@@ -1,4 +1,42 @@
 package com.codegym.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String fullName;
+
+    private String address;
+
+    private String phone;
+
+    private String avatar;
+
+    private String status;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
+    private List<Role> roles;
 }
